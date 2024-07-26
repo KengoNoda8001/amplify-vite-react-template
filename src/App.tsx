f615ac4c6c1b69ab9b0aae468a887311ef5720de
kengo_noda <kengo_noda@example.com>
2024-07-26 13:24:59 +0900

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -26,8 +26,10 @@ function App() {
 
   return (
     <Authenticator>
-      {({ signOut }) => (
+      {({ signOut, user }) => (
+    //  {({ signOut }) => (
     <main>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
